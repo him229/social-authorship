071b8c5133331b838c648f169a647b156195274e
Himank Yadav <him229@gmail.com>
2017-11-22 21:21:49 -0600

    added lexical classification

--- pacling.py
+++ pacling.py
@@ -3,6 +3,18 @@ import string
 import nltk
 from collections import Counter
 import os
+import json
+
+import numpy as np
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.cluster import KMeans
+from scipy.cluster.vq import whiten
+
+
+import statistics
+sentence_tokenizer = nltk.data.load('tokenizers/punkt/english.pickle')
+word_tokenizer = nltk.tokenize.RegexpTokenizer(r'\w+')
 
 class Pacling:
     def __init__(self):
@@ -11,7 +23,9 @@ class Pacling:
     def classify(self, author_string, test_string):
         pa = Counter(nltk.bigrams(author_string))
         pt = Counter(nltk.bigrams(test_string))
+
         total = 0
+
         worst_possible = 0
         for b in pa:
             f1 = pa[b] / len(author_string)  # I think this will normalize fine
@@ -24,22 +38,68 @@ class Pacling:
         return abs(1 - (total / worst_possible))
 
 
+        for key in set(pa.keys() + pt.keys()):
+            f1 = pa[key]
+            f2 = pt[key]
+            total += (2 * (f1 - f2) / (f1 + f2)) ** 2
+        return total
+
 pacling = Pacling()
-# print pacling.classify("tacosa", "salad")
-# print pacling.classify("salad", "salad")
-# print pacling.classify("yeah buddy", "yeah poop")
-# print pacling.classify("vrckid is the best", "vrckid is the worst")
-# print pacling.classify("sophia", "denice")
-# print pacling.classify(" whoa whoa whoa whoa whoa ", " whoa ")
 
+def lexical_stuff(comments):
+        # print pacling.classify('\n'.join(sahil), '\n'.join(sahil))
+        comments = filter(lambda k: k != "", comments)
 
-#print pacling.classify("Hurricane pike is instant at least so the storm can't zip away like he can with the autos projectile. Commenting on this late but I'm a senior CS major and took 208 on campus and I can honestly say it's one of my top 3 class. Learned an absolute shitton and made me better at problem solving.", "This post was removed due to breaking our giveaway rules\n\n>Giveaways are sometimes allowed if they're simply accompanying a post that has a different purpose to it, with the giveaway on the side (and not in the submission title).")
+        fvs_lexical = np.zeros((len(comments), 3), np.float64)
+        fvs_punct = np.zeros((len(comments), 3), np.float64)
+        for e, ch_text in enumerate(comments):
+            tokens = nltk.word_tokenize(ch_text.lower())
+            words = word_tokenizer.tokenize(ch_text.lower())
+            sentences = sentence_tokenizer.tokenize(ch_text)
+            vocab = set(words)
+            words_per_sentence = np.array([len(word_tokenizer.tokenize(s)) for s in sentences])
 
-list_users = os.listdir('./data')
-list_users = [a[:-5] for a in list_users]
-print list_users
-# with open('./data/vrckid.data', 'r') as f:
-#     data = f.read()
-#     import json
-#     data = json.loads(data)
-#     print pacling.classify('\n'.jo(data[:len(data)/2]), '\n'.join(data[len(data)/2:]))
+            fvs_lexical[e, 0] = words_per_sentence.mean()
+            fvs_lexical[e, 1] = words_per_sentence.std()
+            if words:
+                fvs_lexical[e, 2] = len(vocab) / float(len(words))
+            if sentences:
+                fvs_punct[e, 0] = tokens.count(',') / float(len(sentences))
+                fvs_punct[e, 1] = tokens.count(';') / float(len(sentences))
+                fvs_punct[e, 2] = tokens.count(':') / float(len(sentences))
+    
+        # apply whitening to decorrelate the features
+        fvs_lexical = whiten(fvs_lexical)
+        fvs_punct = whiten(fvs_punct)
+
+        return np.nan_to_num(fvs_lexical), np.nan_to_num(fvs_punct)
+
+        # # average number of words per sentence
+        # words_per_sentence_avg = sum(words_per_sentence) / float(len(words_per_sentence))
+        # words_per_sentence_deviation = statistics.stdev(words_per_sentence)
+        # lexical_diversity = len(vocab) / float(len(words))
+
+        # comma_punct = tokens.count(',') / float(len(sentences))
+        # semi_colon_punct = tokens.count(';') / float(len(sentences))
+        # colon_punct = tokens.count(':') / float(len(sentences))
+
+def predict_authors(fvs):
+    km = KMeans(n_clusters=2, init='k-means++', n_init=10, verbose=0)
+    km.fit(fvs)
+    return km
+
+with open('./data/vrckid.data', 'r') as f, open('./data/dick-nipples.data', 'r') as f2:
+    sahil = f.read()
+    elaus = f2.read()
+    sahil = json.loads(sahil)
+    elaus = json.loads(elaus)
+    comments = sahil + elaus
+    
+feature_sets = list(lexical_stuff(comments))
+
+classifications = [predict_authors(fvs).labels_ for fvs in feature_sets]
+results = classifications[0]
+res_list = []
+for _ in range(10):
+    res_list.append((sum(results[:len(sahil)])/float(len(sahil))))
+print "Accuracy = " + str(np.median(res_list))
